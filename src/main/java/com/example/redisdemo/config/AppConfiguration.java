5c365f19e22d39a34576e0f2587744bb4aa8b1b8
Rufet Huseynov <103166770+ruhuseyn@users.noreply.github.com>
2023-02-09 20:32:29 +0400

    Some Changes

--- src/main/java/com/example/redisdemo/config/AppConfiguration.java
+++ src/main/java/com/example/redisdemo/config/AppConfiguration.java
@@ -2,6 +2,7 @@ package com.example.redisdemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -9,6 +10,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 public class AppConfiguration {
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("localhost",6379);
         return new JedisConnectionFactory();
     }
 
